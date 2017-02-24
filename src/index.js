e5390415ec584d5e0b409a855de0cdfa391abfc8
Justyna Foster <justynawfoster@gmail.com>
2017-02-23 20:16:27 -0700

    App, SearchBar Component with state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+
+const API_KEY = 'AIzaSyArUQoC6GWq4k9Tq2S7udFScUhlFE6UqfU';
+
 const App = () => {
-  return <div>Hi!</div>; 
+  return (
+    <div>
+  <SearchBar/>
+  </div>
+  );
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container')); 
